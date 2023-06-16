1b9330e7d3dc1485727d43ec26cbad26e0154583
CapCutPro <136860774+CapCutPro@users.noreply.github.com>
2023-06-17 04:27:21 +0500

    Add files via upload

---
+++ link.php
@@ -0,0 +1,17 @@
+<head>
+</head>
+<body>
+<p>Lovely <a href="https://procapcut.net/"> Latest CapCut Template</a> is created by Kendrick Lamar. It is most viral template and people are crazy for it as its beat changes the images and add slomo. This effect looks very charming. Music sync of this template is also great. You can find here 9 templates of this type, do select and click on use Template on CapCut button to go to CapCut app for making the video.</p>
+
+<?php
+// Example PHP code
+$currentDate = date("Y-m-d");
+echo "<p>Today's date is: $currentDate</p>";
+
+$number1 = 10;
+$number2 = 5;
+$sum = $number1 + $number2;
+echo "<p>The sum of $number1 and $number2 is: $sum</p>";
+?>
+
+</body>
